23a7090762c385876e91a3c3166ba02b2c1d7e71
Lex <lex.bruynooghe@gmail.com>
2024-08-05 07:42:15 +0200

    renamed

--- discord_bot.py
+++ discord_bot.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
@@ -16,12 +16,12 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 
-@bot.command(name='here')
-async def set_target_channel(ctx):
+@bot.command(name="here")
+async def set_target_channel(ctx: discord.ext.commands.context.Context):
     target_channel_id = ctx.channel.id
     print(ctx)
     print(target_channel_id)
     await ctx.send(f'Target channel set to {ctx.channel.name}')
 
-
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 bot.run(DISCORD_TOKEN)
