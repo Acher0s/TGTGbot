de00c28f7086a45f4cab88c3ebc8ab0fc2ef7ef1
Lex <lex.bruynooghe@gmail.com>
2024-08-05 19:24:50 +0200

    checkpoint

--- tgtg_client.py
+++ tgtg_client.py
@@ -1,4 +1,5 @@
 import os
+import location_util
 
 from tgtg import TgtgClient
 
@@ -11,9 +12,25 @@ class TGTG:
         self.client = TgtgClient(email=os.getenv("TGTG_EMAIL"))
         self.credentials = self.client.get_credentials()
 
-    def test(self):
-        self.client.get_items()
+    def get_items(self, loc: location_util.Location, radius=10):
+        return self.client.get_items(
+            latitude=loc.latitude,
+            longitude=loc.longitude,
+            radius=radius,
+            favorites_only=False,
+            page_size=400,
+        )
 
 
 if __name__ == "__main__":
     client = TGTG()
+
+    while True:
+        i = 0
+
+        location = location_util.get_location_from_string(input("enter location: "))
+        items = client.get_items(location)
+        file = open(f"./items/items{i}.json", "a", encoding="utf-8")
+        file.write(str(items))
+        file.close()
+        i += 1
